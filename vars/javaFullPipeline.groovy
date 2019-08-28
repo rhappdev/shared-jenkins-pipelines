41719c75626108c585505c500930013c26b6bc04
Mikel Sanchez Herrero <misanche@redhat.com>
2019-08-28 19:46:27 +0200

    Update Typo

--- vars/javaFullPipeline.groovy
+++ vars/javaFullPipeline.groovy
@@ -1,6 +1,6 @@
 def call(Map pipelineParameters) {
 
-   echo "Executing full CI/CD Java pipeline for application ${pipelineParameters.appName}: Git server (${pipelineParameters.gitUrl}) and branch (${pipelineParameters.gitBranch})""
+   echo "Executing full CI/CD Java pipeline for application ${pipelineParameters.appName}: Git server (${pipelineParameters.gitUrl}) and branch (${pipelineParameters.gitBranch})"
 
    mavenCI(pipelineParameters)
 
